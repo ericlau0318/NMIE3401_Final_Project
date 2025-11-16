a8e28d87ffcc27b62275b35a2699ae9708381d54
ericlau0318 <ericlau0318@gmail.com>
2025-11-17 00:47:03 +0800

    jump

--- Assets/NetworkPlayerController.cs
+++ Assets/NetworkPlayerController.cs
@@ -8,22 +8,47 @@ public class NetworkPlayerController : NetworkBehaviour
 {
 
     private PlayerInputAction pActions;
-    [SerializeField]
-    float moveSpeed;
+    [SerializeField] float moveSpeed=1f;
+    [SerializeField] float jumpForce=5f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float CheckRadius = 0.2f;
+    [SerializeField] LayerMask groundLayer=1 << 6;
+
     private Vector2 finalMove;
+    private Rigidbody2D rb;
+    private bool isGrounded;
     void Start()
     {
+
         pActions = new PlayerInputAction();
         pActions.Enable();
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { 
         if (!IsOwner) return;
 
         var move = pActions.Player.Move.ReadValue<Vector2>();
-        finalMove = new Vector3(move.x, 0,0) * Time.deltaTime * moveSpeed;
-        transform.Translate(move, Space.World);
+        finalMove = new Vector2(move.x, 0) * moveSpeed; 
+
+        rb.velocity = new Vector2(finalMove.x, rb.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, CheckRadius, groundLayer);
+
+        if (pActions.Player.Jump.IsPressed() && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        
+
+        if (isGrounded)
+        {
+            //Debug.Log("touching ground");
+        }
+
     }
+
 }
